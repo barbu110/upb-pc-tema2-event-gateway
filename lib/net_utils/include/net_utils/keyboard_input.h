7294d2c28199a6da87dbde25a401bdb87ace0b74
Victor Barbu <victorbarbu08@gmail.com>
2020-04-17 21:05:56 +0300

    Implement KeyboardInput event source
    The KeyboardInput event source allows users to retrieve input from STDIN
    as soon as it arrives. By default, the event source creates an
    std::string containing the input data without the trailing newline, if
    there was any.
    
    This event source is now used to allow users of the Gateway server to
    type "exit" in the terminal and terminate the server gracefully.

---
+++ lib/net_utils/include/net_utils/keyboard_input.h
@@ -0,0 +1,103 @@
+#pragma once
+
+#include "microloop/buffer.h"
+#include "microloop/event_source.h"
+
+#include <fcntl.h>
+#include <string>
+#include <tuple>
+#include <unistd.h>
+
+namespace net_utils
+{
+
+class KeyboardInput : public microloop::EventSource, protected microloop::TypeHelper<std::string>
+{
+public:
+  KeyboardInput(bool remove_trailing_newline = true) :
+      EventSource{STDIN_FILENO}, remove_trailing_newline_{remove_trailing_newline}
+  {
+    auto flags = fcntl(get_fd(), F_GETFL);
+    if (flags == -1)
+    {
+      throw microloop::KernelException(errno);
+    }
+
+    flags |= O_NONBLOCK;
+    if (fcntl(get_fd(), F_SETFL, flags) == -1)
+    {
+      throw microloop::KernelException(errno);
+    }
+  }
+
+  template <class Func, class... Args>
+  void on_input(Func &&func, Args &&... args)
+  {
+    using namespace std::placeholders;
+    on_input_ = std::bind(std::forward<Func>(func), std::forward<Args>(args)..., _1);
+  }
+
+  std::uint32_t produced_events() const override
+  {
+    return EPOLLIN;
+  }
+
+  bool native_async() const override
+  {
+    /* Run this event source on the main thread, so reading the input can block. */
+    return true;
+  }
+
+  void start() override
+  {}
+
+  void run_callback() override
+  {
+    read_input();
+    std::apply(on_input_, get_return_object());
+  }
+
+private:
+  void read_input()
+  {
+    /*
+     * Keyboard inputs are generally expected to be relatively small, so reducing the amount of
+     * memory to allocate is feasible.
+     */
+    char buf[512]{};
+
+    std::string input;
+    input.reserve(sizeof(buf));
+
+    ssize_t nread;
+
+    do
+    {
+      nread = read(STDIN_FILENO, buf, sizeof(buf));
+      if (nread == -1)
+      {
+        if (errno == EAGAIN || errno == EWOULDBLOCK)
+        {
+          break;
+        }
+
+        throw microloop::KernelException{errno};
+      }
+
+      input.append(buf, nread);
+    } while (nread != 0);
+
+    if (remove_trailing_newline_ && input.back() == '\n')
+    {
+      input.pop_back();
+    }
+
+    set_return_object(input);
+  }
+
+private:
+  Callback on_input_;
+  bool remove_trailing_newline_;
+};
+
+}  // namespace net_utils
--- main/gateway_server.cpp
+++ main/gateway_server.cpp
@@ -1,11 +1,23 @@
-#include "gateway/gatewway.h"
+#include "gateway/gateway.h"
 #include "microloop/event_loop.h"
+#include "net_utils/keyboard_input.h"
 
 #include <iostream>
+#include <signal.h>
 
 int main()
 {
   gateway::Gateway gateway{8900};
+
+  auto keyboard_input = new net_utils::KeyboardInput;
+  keyboard_input->on_input([](const std::string &data) {
+    if (data == "exit")
+    {
+      kill(getpid(), SIGINT);
+    }
+  });
+
+  microloop::EventLoop::instance().add_event_source(keyboard_input);
 
   while (MICROLOOP_TICK())
   {}
